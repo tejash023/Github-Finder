b594d098463bb0da7a9011453d2b7eb09902b28d
Tejashwa Rajvardhan <keshu023@gmail.com>
2022-08-04 13:19:26 +0530

    ui changes

--- github.js
+++ github.js
@@ -1,7 +1,7 @@
 class Github{
   constructor(){
-    this.client_id ='d587162e7e9e23cdc3b1';
-    this.client_secret = 'acbdbe8841d1b9cfec4d6f2c9d5c43778975d4bd';
+    this.client_id = config.MY_API_TOKEN;
+    this.client_secret = config.SECRET_API_KEY;
     this.repos_count = 5;
     this.repos_sort = 'created: asc';
   }
@@ -9,7 +9,7 @@ class Github{
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
     const profile = await profileResponse.json();
     const repos = await reposResponse.json();
