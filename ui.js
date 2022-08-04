290f536f100b674b1442441153080160e4dc291d
Tejashwa Rajvardhan <keshu023@gmail.com>
2022-08-04 13:02:18 +0530

    updated ui.js

--- ui.js
+++ ui.js
@@ -32,7 +32,30 @@ class UI{
     `;
   }
 
+  //show user repos
+  showRepos(repos){
+    let output = '';
 
+    repos.forEach((repo) => {
+      output += `
+      <div class = "card card-body mb-2">
+        <div class = "row">
+          <div class = "col-md-6">
+            <a href = "${repo.html_url}" target = "_blank">${repo.name}</a>
+          </div>
+          <div class = "col-md-6">
+            <span class = "badge bg-primary"> Stars: ${repo.stargazers_count}</span>
+            <span class = "badge bg-warning"> Watchers: ${repo.watchers_count}</span>
+            <span class = "badge bg-success"> Forks: ${repo.forms_count}</span>
+          </div>
+        </div>
+      </div>
+      `;
+    });
+
+    //output repos
+    document.getElementById('repos').innerHTML = output;
+  }
 
   //show alert for profile not found
   showAlert(message, className){
